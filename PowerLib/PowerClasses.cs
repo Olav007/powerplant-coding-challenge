d36d017aff0283a458e326a6053583d20cdad034
Olav007 <git@orgt.eu>
2023-11-11 21:01:39 +0100

    Initial

---
+++ PowerLib/PowerClasses.cs
@@ -0,0 +1,41 @@
+﻿namespace PowerLib
+{
+
+
+    public class Fuel
+    {
+        public double Gas { get; set; }
+        public double Kerosine { get; set; }
+        public double Co2 { get; set; }
+        public double Wind { get; set; }
+    }
+
+    public class PowerPlant
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public double Efficiency { get; set; }
+        public int Pmin { get; set; }
+        public int Pmax { get; set; }
+    }
+
+    public class PowerSystem
+    {
+        public int Load { get; set; }
+        public Fuel Fuels { get; set; }
+        public List<PowerPlant> PowerPlants { get; set; }
+    }
+
+    public class PowerPlantPlan
+    {
+        public string Name { get; set; }
+        public int P { get; set; }
+    }
+
+    public class PowerPlan
+    {
+        public List<PowerPlantPlan> PowerPlantPlans { get; set; }
+    }
+
+}
+
---
+++ PowerServiceAPI/Controllers/productionplanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using PowerLib;
+
+namespace PowerServiceAPI.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class productionplanController : ControllerBase
+    {
+        private const string PowerServiceAPI_HostAddress = "https://localhost:8888";
+
+        [HttpPost]
+        public PowerPlan Post([FromBody] PowerLib.PowerSystem value)
+        {
+            return new PowerPlan();
+
+        }
+
+    }
+}
