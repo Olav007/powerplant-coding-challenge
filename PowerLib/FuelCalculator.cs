740f45abf5dde7465df9173b283d7b7851b4ae77
Olav007 <git@orgt.eu>
2023-11-12 22:31:19 +0100

    Woking for sample payload

--- PowerLib/FuelCalculator.cs
+++ PowerLib/FuelCalculator.cs
@@ -6,17 +6,34 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
-namespace Powerlib
+namespace PowerLib
 {
-    internal static class FuelCalculator
+    using Fuels = System.Text.Json.JsonElement;
+    internal class FuelCalculator
     {
-        static public double price( Fuels fuels,string plantType)
+        double? gas;
+        double? kerosine;
+        double? wind;
+        public FuelCalculator( Fuels fuels)
+        {
+            foreach( var fuel in fuels.EnumerateObject())
+            {
+                if( fuel.Name.Contains("gas"))
+                    gas = fuel.Value.GetDouble();
+                if (fuel.Name.Contains("kerosine"))
+                    kerosine = fuel.Value.GetDouble();
+                if (fuel.Name.Contains("wind"))
+                    wind = fuel.Value.GetDouble();
+            }
+
+        }
+        public (double price, double percentage) getValue( string plantType)
         {
             if (plantType.Contains("wind"))
-                return 0;
+                return ( 0, wind.Value);
             if( plantType.Contains("gas" ))
-                return fuels.Gas;
-            return fuels.Kerosine;
+                return (gas.Value, 0);
+            return (kerosine.Value, 0);
         }
     }
 }
--- PowerLib/PowerClasses.cs
+++ PowerLib/PowerClasses.cs
@@ -1,13 +1,9 @@
-﻿namespace PowerLib
-{
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
-    public class Fuels
-    {
-        public double Gas { get; set; }
-        public double Kerosine { get; set; }
-        public double Co2 { get; set; }
-        public double Wind { get; set; }
-    }
+namespace PowerLib
+{
+    using Fuels = System.Text.Json.JsonElement;
 
 
     public class PowerPlant
@@ -22,20 +18,7 @@
     public class PowerSystem
     {
         public int load { get; set; }
-        //public List<(string, double)> fuels { get; set; }
         public Fuels fuels { get; set; }
         public List<PowerPlant> powerplants { get; set; }
     }
-
-    public class PowerPlantPlan
-    {
-        public string name { get; set; }
-        public int p { get; set; }
-    }
-
-    public class PowerPlan
-    {
-        public List<PowerPlantPlan> PowerPlantPlans { get; set; }
-    }
-
 }
