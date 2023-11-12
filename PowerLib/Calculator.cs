822559ed2a9577a58ef95fdd2b5232e044958d70
Olav007 <git@orgt.eu>
2023-11-12 22:43:58 +0100

    Added Missing

---
+++ PowerLib/Calculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerLib
+{
+    static public class Calculator
+    {
+        public static dynamic Calculate(PowerSystem req)
+        {
+            var plants = new List<(PowerPlant plant, double cost, double percentage)>();
+            var fuelCalculator = new FuelCalculator(req.fuels);
+            foreach (var plant in req.powerplants)
+            {
+                var fuelValue = fuelCalculator.getValue(plant.type);
+                var price = fuelValue.price;
+                var cost = (0 == price) ? 0 : price / plant.efficiency;
+                plants.Add((plant, cost, fuelValue.percentage));
+            }
+            var plantsMeritOrder = plants.OrderBy(plant => plant.cost).ThenBy(plant => plant.plant.name).ToList();
+            var remainingPower = req.load;
+            var res = new List<(double p, PowerPlant plant)>();
+            double requiredPowerRemaining = req.load;
+            foreach( var plant  in plantsMeritOrder)
+            {
+                double p;
+                var pmax = plant.cost > 0 ? plant.plant.pmax : plant.plant.pmax * plant.percentage/100;
+                var pmin = plant.cost > 0 ? plant.plant.pmin : plant.plant.pmin * plant.percentage/100;
+                if ( requiredPowerRemaining < pmin)
+                {
+                    p = 0;
+                }
+                else     if ( pmax < requiredPowerRemaining)
+                {
+                    p = pmax;
+                }
+                else if(requiredPowerRemaining > pmin)
+                {
+                    p = requiredPowerRemaining;
+                }
+                else
+                {
+                    p = pmin;
+                }  
+                res.Add((p, plant.plant));
+                requiredPowerRemaining -= p;
+            }
+            return res.Select(plant => new { name = plant.plant.name, p = plant.p.ToString("F1") }).ToList();
+        }
+    }
+}
---
+++ PowerLib/PowerPlant.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PowerLib
+{
+    internal class PowerPlantPlanRes
+    {
+        private PowerPlant powerPlant;
+        private double merit; 
+        PowerPlantPlanRes(PowerPlant powerPlant_arg)
+        {             
+            powerPlant = powerPlant_arg;
+
+        }
+
+    }
+}
